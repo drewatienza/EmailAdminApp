42214490752831abf0940bc19babdfcf9e1654b2
drewatienza <atienza.drew@gmail.com>
2018-05-10 09:30:40 -0700

    created the outline for the email admin app

---
+++ src/com/drewatienza/Email.java
@@ -0,0 +1,23 @@
+package com.drewatienza;
+
+public class Email {
+    private String firstName;
+    private String lastName;
+    private String password;
+    private String department;
+    private int mailboxCapacity;
+    private String alternateEmail;
+
+    // Constructor to receive first name and last name
+    
+
+    // Ask for the department
+
+    // Generate a random password
+
+    // Set the mailbox capacity
+
+    // Set the alternative email
+
+    // Change the password
+}
