10a52937d8d82ee3dcce9f3fcb5a8775860f333b
drewatienza <atienza.drew@gmail.com>
2018-05-10 11:28:06 -0700

    defined the email department

--- src/com/drewatienza/Email.java
+++ src/com/drewatienza/Email.java
@@ -1,5 +1,7 @@
 package com.drewatienza;
 
+import java.util.Scanner;
+
 public class Email {
     private String firstName;
     private String lastName;
@@ -13,9 +15,34 @@ public class Email {
         this.firstName = firstName;
         this.lastName = lastName;
         System.out.println("EMAIL CREATED: " + this.firstName + " " + this.lastName);
+
+        // Call a method asking for the department - return the department
+        this.department = setDepartment();
+        System.out.println("Department: " + this.department);
     }
 
     // Ask for the department
+    private String setDepartment() {
+        System.out.println("DEPARTMENT CODE:\n" +
+                "1 for Sales\n" +
+                "2 for Development\n" +
+                "3 for Accounting\n" +
+                "0 for none\n" +
+                "Enter department code: ");
+        Scanner in = new Scanner(System.in);
+        int deptChoice = in.nextInt();
+        switch (deptChoice) {
+            case 1:
+                return "sales";
+            case 2:
+                return "dev";
+            case 3:
+                return "acct";
+            default:
+                return "none";
+        }
+    }
+
 
     // Generate a random password
 
