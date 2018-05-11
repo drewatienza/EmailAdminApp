3ccd63f64fce1d2e0ae03710447821b8f40e562b
drewatienza <atienza.drew@gmail.com>
2018-05-11 10:53:20 -0700

    created the email address syntax

--- src/com/drewatienza/Email.java
+++ src/com/drewatienza/Email.java
@@ -7,9 +7,11 @@ public class Email {
     private String lastName;
     private String password;
     private String department;
+    private String email;
     private int mailboxCapacity;
     private int defaultPasswordLength = 10;
     private String alternateEmail;
+    private String companySuffix = "mapt.com";
 
     // Constructor to receive first name and last name
     public Email(String firstName, String lastName) {
@@ -24,6 +26,11 @@ public class Email {
         // Call a method that returns a random password
         this.password = randomPassword(defaultPasswordLength);
         System.out.println("Your password is: " + this.password);
+
+        // Combine elements to generate email
+        email = firstName.toLowerCase() + "." + lastName.toLowerCase() + "@" +
+                department + "." + companySuffix;
+        System.out.println("Your email is: " + email);
     }
 
     // Ask for the department
